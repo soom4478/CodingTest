135e60cdb9a58799dfef28e3807c915fb034a883
soomin <142988772+soom4478@users.noreply.github.com>
2024-03-28 22:31:17 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -4,8 +4,7 @@ class Solution {
         answer = new int[end_num - start_num+1];
         
         for(int i=0; i<answer.length; i++) {
-            answer[i] = start_num;
-            start_num++;
+            answer[i] = start_num++;
         }
         
         return answer;
