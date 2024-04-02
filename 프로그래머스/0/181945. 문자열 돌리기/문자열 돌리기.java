52fd820d46447cabbe5cbe88b2c848f649d9bf0b
soomin <142988772+soom4478@users.noreply.github.com>
2024-04-02 23:13:11 +0900

    [level 0] Title: 문자열 돌리기, Time: 137.65 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,15 +4,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        char c[] = new char[10];    // 제한사항 : 1 <= str <= 10 이므로
+        char c[] = new char[10];
         
         for(int i=0; i<a.length(); i++) {
-            c[i] = a.charAt(i);
+            System.out.println(a.charAt(i));
         }
-        
-        for(int i=0; i<a.length(); i++) {
-            System.out.println(c[i]);
-        }
-        
     }
 }
