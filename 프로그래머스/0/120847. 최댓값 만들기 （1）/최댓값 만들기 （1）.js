35cec33ef89e1736dc60d6ea4111be777b77b8d2
soomin <142988772+soom4478@users.noreply.github.com>
2024-08-09 22:59:12 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,11 +1,4 @@
 function solution(numbers) {
-    var answer = Number.MIN_SAFE_INTEGER;
-    for(let i=0; i<numbers.length; i++) {
-        for(let j=i+1; j<numbers.length; j++) {
-            if(answer < numbers[i]*numbers[j]) {
-                answer = numbers[i]*numbers[j]
-            }
-        }
-    }
-    return answer;
+    numbers.sort((a,b) => b-a);
+    return numbers[0] * numbers[1];
 }
